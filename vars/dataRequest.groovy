e8861915a6f178f6c5411ee7289525231231d018
thanhdatnasdaq <122451214+thanhdatnasdaq@users.noreply.github.com>
2023-03-03 00:47:00 +1100

    Update dataRequest.groovy

--- vars/dataRequest.groovy
+++ vars/dataRequest.groovy
@@ -1,8 +1,7 @@
-def exeQueue = []
 def call(projectName, requiredAgent='java8'){
+def exeQueue = []
 pipeline{
     agent any
-    
     stages{
         stage("Set Up"){
             steps{
@@ -19,7 +18,7 @@ pipeline{
                     sh "env"
                     echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
                     script{                        
-                        readExampleFile()
+                        readExampleFile(exeQueue)
                     }  
                 }
             }
@@ -28,37 +27,37 @@ pipeline{
                 script{
                     parallel(
                         thread1:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread2:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread3:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread4:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread5:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread6:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread7:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread8:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread9:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         },
                         thread10:{
-                            if(!exeQueue.isEmpty()){ executeParallel()}
+                            if(!exeQueue.isEmpty()){ executeParallel(exeQueue)}
                         }
                     )
-                    if(!exeQueue.isEmpty()){ executeHelper}
+                    if(!exeQueue.isEmpty()){ executeHelper(exeQueue)}
                 }
             }
         }
@@ -72,21 +71,22 @@ pipeline{
     }
 }
 
-def readExampleFile(){
+def readExampleFile(List exeQueue){
     def data = readFile("example.txt").readLines()
     for(line in data){
         exeQueue.add(line)
     }
+    print("Complete read file")
 }
 
-def executeParallel(){
+def executeParallel(List exeQueue){
     while(exeQueue.size > 10){
         print(exeQueue[0])
         exeQueue.remove(0)
     }
 }
 
-def executeHelper(){
+def executeHelper(List exeQueue){
     while(exeQueue.size > 0){
         print(exeQueue[0])
         exeQueue.remove(0)
